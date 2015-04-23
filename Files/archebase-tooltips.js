cd80742146946bf3caf7bcf4a61a4b32fc28acc3
pedrobj <pedro.bj@hotmail.com>
2015-04-23 17:38:24 -0300

    1.1
    App with API Based

---
+++ Files/archebase-tooltips.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', function() {
+	var body = document.body,
+		css = function(styles) {
+			for(var i in styles) this.style[i] = styles[i];
+		},
+	fr = document.createElement('iframe');
+	fr.scrolling = 'no';
+	var url = 'arche-base.com/api/tooltip';
+	
+	css.call(fr, {
+		border: 'none',
+		display: 'none',
+		width: '100%',
+		height: '100%',
+		position: 'fixed',
+		left: 0,
+		top: 0,
+		zIndex: 100
+	});
+	
+	fr.scrolling = 'no';
+	
+	body.appendChild(fr);
+	
+	body.onmouseover = function(e) {
+		e = e || window.event;
+		var self =  e.target || e.srcElement;
+		if((self.tagName == 'A' && ~self.href.indexOf(url)) || (self.tagName == 'IMG' && self.parentNode.tagName == 'A' && ~self.parentNode.href.indexOf(url))) {
+		
+			fr.style.left = e.clientX + 20 + 'px';
+			fr.style.top = e.clientY + 10 + 'px';
+			
+			fr.style.display = 'block';
+			/*
+			fr.onload = function() {
+				this.style.display = 'block';				
+			}
+			*/
+			
+			fr.src = (self.href || self.parentNode.href) + '?tooltip=load';
+			
+			self.onmousemove = function(e) {
+				e = e || window.event;
+				fr.style.left = e.clientX + 20 + 'px';
+				fr.style.top = e.clientY + 10 + 'px';
+			}
+					
+			self.onmouseout = function() {
+				this.onmouseover = null;
+				this.onmousemove = null;
+				fr.src = '';
+				fr.onload = null;
+				fr.style.display = 'none';
+			}
+		}
+	}
+}, false);
---
+++ Files/feed.js
@@ -0,0 +1,102 @@
+var archebaseFeed=new function(){
+	var options = {
+		element:$('#feed-content'),
+		type: 'rising',
+		page: 1,
+		limit: 5
+	};
+	
+	this.init=function(opts){
+		options=$.extend(options,opts);
+		if(!options.element){
+			console.log('Element not provided');
+			return false;
+		}
+		this.get();
+	}
+	
+	this.get=function(){
+		options.element.find("#page-"+options.page).hide();
+		
+		//http://arche-base.com/api/builds/'+options.type+'/page:'+options.page+'/direction:desc?script=true&limit='+options.limit+'&page='+options.page+'order='+options.type
+		$.ajax({
+			url: 'http://arche-base.com/api/builds?script=true&limit='+options.limit+'&page='+options.page+'&type='+options.type,
+			dataType: 'jsonp',
+			crossDomain: true,
+			jsonpCallback: 'archebaseFeed.load',
+			cache:true
+		});
+	};
+	
+	this.load=function(r){
+		window.data = $.extend({},r); //Global Data
+
+		this.drawFeed();
+	
+		$("._feed-page").button('reset').fadeIn('slow');
+		options.element.find("#page-"+options.page).fadeIn('slow');
+	}
+	
+	this.drawFeed=function(){
+		options.element.append(
+			$("<div>")
+				.attr('data-page', options.page)
+				.attr('id', 'page-'+options.page)
+		);
+		
+		$.each(data.builds, function(key, build){
+			options.element.find("#page-"+options.page).append(formatFeed(build));
+			
+			var dataTree = [build.Tree1, build.Tree2, build.Tree3];
+			for(var tree in dataTree){
+				if(dataTree[tree].Skills.length > 0){
+					$(".feed-row:last").find('.feed-las-t'+(parseInt(tree)+1)).append(
+						$('<i>')
+							.addClass('icon min t'+dataTree[tree].id)
+							.attr('title', dataTree[tree].name)
+					);
+					for(var i in dataTree[tree].Skills){
+						$(".feed-row:last").find('.feed-las-t'+(parseInt(tree)+1)).append(
+							$('<a>')
+								.css('background-image', 'url('+dataTree[tree].Skills[i].icon+')')
+								.attr('href', dataTree[tree].Skills[i].tooltip)
+								.attr('data-tp', 'TODO')
+								.addClass('tp-base spell-icon')
+						);
+					}
+				}
+			}
+			
+			var content = buildContentInfo(build.Build.content);
+			$(".feed-row:last").find('.glyphicon-text-width').css('color', content.color).attr('title', content.name);
+			
+			if(build.Build.votes_rising > 0)
+				$(".feed-row:last").find(".feed-rising").show(0);
+		});
+	}
+	
+	function formatFeed(build){
+		var feedTemplate = $($('#feed-template'));
+		return feedTemplate.html()
+			.replace(/\{(.+?)\}/g, function($0, $1) {
+				return eval($1);
+			});
+	}
+	
+	function buildContentInfo(value){
+		if(value > 15000)
+			return {name:'Artifact', color:'#e167a8'};
+		else if(value > 10000)
+			return {name:'Epic', color:'#7133ff'};
+		else if(value > 5000)
+			return {name:'Excellent', color:'#67a8e1'};
+		else if(value > 1500)
+			return {name:'Good', color:'#a7e065'};
+		else if(value > 0)
+			return {name:'Average', color:'#FFF'};
+		else if(value == 0) 
+			return {name:'Inferior', color:'silver'};
+		else
+			return {name: 'Inferior', color: 'silver'};
+	}
+}
